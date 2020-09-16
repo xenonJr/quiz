348a9ad0db9d109f0471be0787910dc3279e20c3
xenonjr <zkxenon98@gmail.com>
2020-09-17 04:25:00 +0600

    first

--- app/src/main/java/com/example/quizapp/Splash.java
+++ app/src/main/java/com/example/quizapp/Splash.java
@@ -10,6 +10,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        System.out.println("sdas");
     }
 
 }
